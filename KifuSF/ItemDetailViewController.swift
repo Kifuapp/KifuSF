7427d69a58456006103fde8997adba4d5e261568
LinnierGames <esericksanc@gmail.com>
2018-07-29 10:54:47 -0700

    ItemDetailedVc

--- KifuSF/ItemDetailViewController.swift
+++ KifuSF/ItemDetailViewController.swift
@@ -9,42 +9,79 @@
 import UIKit
 
 class ItemDetailViewController: UIViewController {
-
+    
+    var donation: Donation!
+    
+    /** this also disables the view's isUserInteractive */
+    private var isRequestButtonEnabled: Bool {
+        set {
+            requestButtonView.alpha = newValue ? 1.0 : 0.45
+            requestButtonView.isUserInteractionEnabled = newValue
+            view.isUserInteractionEnabled = newValue
+            barButtonDone.isEnabled = newValue
+        }
+        get {
+            return view.isUserInteractionEnabled
+        }
+    }
+    
+    // MARK: - RETURN VALUES
+    
+    // MARK: - VOID METHODS
+    
+    private func updateUI() {
+        itemImage.kf.setImage(with: URL(string: donation.imageUrl)!)
+        itemName.text = donation.title
+        //TODO: distance
+//        itemDistance.text =
+        postDetail.text = donation.donator.username
+        descriptionView.text = donation.notes
+    }
+    
+    // MARK: - IBACTIONS
+    
     @IBOutlet weak var itemName: UILabel!
     @IBOutlet weak var itemDistance: UILabel!
     @IBOutlet weak var postDetail: UILabel!
     @IBOutlet weak var descriptionView: UITextView!
     @IBOutlet weak var itemImage: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    @IBOutlet weak var requestButtonTapped: UIButton!
+    @IBOutlet weak var requestButtonView: UIView!
+    @IBOutlet weak var barButtonDone: UIBarButtonItem!
     
     @IBAction func cancelButtonTapped(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+        self.navigationController!.popViewController(animated: true)
     }
     
     @IBAction func requestButtonTapped(_ sender: Any) {
-        //TODO
+        self.isRequestButtonEnabled = false
+        
+        RequestService.createRequest(for: donation) { success in
+            if success {
+                self.navigationController!.popViewController(animated: true)
+            } else {
+                let alert = UIAlertController(errorMessage: nil)
+                self.present(alert, animated: true)
+                
+                self.isRequestButtonEnabled = true
+            }
+        }
     }
     
-    /*
-     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - LIFE CYCLE
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController!.setNavigationBarHidden(true, animated: true)
+        
+        updateUI()
     }
-    */
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController!.setNavigationBarHidden(false, animated: true)
+    }
 
 }
--- KifuSF/ItemListViewController.swift
+++ KifuSF/ItemListViewController.swift
@@ -15,13 +15,26 @@ class ItemListViewController: UIViewController {
             postTable.reloadData()
         }
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            switch identifier {
+            case "show detailed donation":
+                guard
+                    let cell = sender as? UITableViewCell,
+                    let indexPath = postTable.indexPath(for: cell),
+                    let vc = segue.destination as? ItemDetailViewController else {
+                        fatalError("storyboard not set up correctly")
+                }
+                
+                let selectedDonation = openDonations[indexPath.row]
+                vc.donation = selectedDonation
+            default: break
+            }
+        }
+    }
 
     @IBOutlet weak var postTable: UITableView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -30,22 +43,6 @@ class ItemListViewController: UIViewController {
             self?.openDonations = donations
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
@@ -64,23 +61,9 @@ extension ItemListViewController: UITableViewDataSource {
         postCell.itemImage.kf.setImage(with: URL(string: donation.imageUrl)!)
         postCell.distance.text = "\(1.0) miles from here"
         postCell.postInfo.text = "@\(donation.donator.username)"
-        
-        postCell.delegate = self
         return postCell
     }
 }
 
 extension ItemListViewController: UITableViewDelegate {
-    
-}
-
-extension ItemListViewController: ItemPostCellDelegate {
-    func requestButtonTapped(cell: ItemPostCell) {
-        guard let indexPath = postTable.indexPath(for: cell) else {
-            return assertionFailure("index path not found")
-        }
-        
-        let selectedDonation = openDonations[indexPath.row]
-        RequestService.createRequest(for: selectedDonation)
-    }
 }
