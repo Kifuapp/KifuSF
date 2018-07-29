b3f0c299f487771e098f2b462642552f368ad581
Shupro <shupro1226@gmail.com>
2018-07-28 19:05:47 -0700

    set segue , made new donation view (not done)

---
+++ KifuSF/StatusViewController.swift
@@ -0,0 +1,34 @@
+//
+//  StatusViewController.swift
+//  KifuSF
+//
+//  Created by Shutaro Aoyama on 2018/07/28.
+//  Copyright © 2018年 Alexandru Turcanu. All rights reserved.
+//
+
+import UIKit
+
+class StatusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
