ba7f96cebd8f35e4ffbb7e7193b83d6cd130ebf5
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2019-01-04 16:21:52 +0200

    New tab bar settings button
    + settings icon

--- KifuSF/SettingsViewController.swift
+++ KifuSF/SettingsViewController.swift
@@ -23,6 +23,7 @@ class SettingsViewController: UIViewController {
                           body: "Don't be shy 😉"),
         InformationModel(cellTitle: "Terms of Service", information: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
     ]
+    
 //    SettingsItemModel(name: "Donation Regulations"),
 //    SettingsItemModel(name: "Terms of Service"),
 //    SettingsItemModel(name: "Privacy Policy"),
@@ -33,6 +34,7 @@ class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureData()
         configureDelegates()
         configureStyling()
         configureLayout()
