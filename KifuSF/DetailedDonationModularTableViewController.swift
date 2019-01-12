6c4b2eb03e9da4799ba37a726307def31a56d9ae
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2019-01-12 22:23:26 +0200

    Fixed Todos from DetailedDonationModularTableViewController

--- KifuSF/DetailedDonationModularTableViewController.swift
+++ KifuSF/DetailedDonationModularTableViewController.swift
@@ -73,10 +73,10 @@ class DetailedDonationModularTableViewController: ModularTableViewController {
             imageURL: imageUrl,
             title: donation.title,
             username: donator.username,
-            creationDate: String(describing: donation.creationDate), //TODO: format date
-            userReputation: 22, //TODO: alex-reputation
-            userDonationsCount: 12, //TODO: alex-reputation
-            userDeliveriesCount: 12, //TODO: alex-reputation
+            creationDate: donation.creationDate.stringValue(),
+            userReputation: donator.reputation,
+            userDonationsCount: donator.numberOfDonations,
+            userDeliveriesCount: donator.numberOfDeliveries,
             distance: distance,
             description: donation.notes
         )
